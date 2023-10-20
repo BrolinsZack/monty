385ef6ba2e812184fc1efe9a7b2a15387a6d66ba
BrolinsZack <zackbrolins@gmail.com>
2023-10-20 00:40:53 -0700

    two files

--- pall.c
+++ pall.c
@@ -1,24 +1,21 @@
 #include "monty.h"
 /**
- * f_pall - Function tha Prints the stack.
- *
- * @head: Stack head.
- *
- * @counter: None used.
- *
- * Return: No return.
+ * f_pall - prints the stack
+ * @head: stack head
+ * @counter: no used
+ * Return: no return
 */
 void f_pall(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
-	(void)counter;
+        stack_t *h;
+        (void)counter;
 
-	h = *head;
-	if (h == NULL)
-		return;
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
+        h = *head;
+        if (h == NULL)
+                return;
+        while (h)
+        {
+                printf("%d\n", h->n);
+                h = h->next;
+        }
 }
